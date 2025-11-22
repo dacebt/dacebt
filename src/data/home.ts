4c61db1aa9d7c7ba1f3478d2eefdec811c6f5590
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-11-22 00:14:49 -0500

    refactor: update welcome messages with more detailed introduction and personality; expand navigation instructions to include about page

--- src/data/home.ts
+++ src/data/home.ts
@@ -2,20 +2,20 @@ import { type DialogueMessage } from "../hooks/useSimpleDialogue"
 
 export const welcomeMessages: DialogueMessage[] = [
   {
-    message: "Welcome to my portfolio!",
+    message: "Welcome to my portfolio! My name is David Colon and I am a web developer!",
     speaker: "David",
     image: "/images/avatar.png",
     imagePosition: "left",
   },
   {
-    message: "I'm a software engineer with a passion for building great products.",
+    message: "I could go on and on about my passion for development, or my obsession with using the latest ai tools to 10x my creations and ideas. But let's save that for the proper pages.",
     speaker: "David",
     image: "/images/avatar.png",
     imagePosition: "right",
   },
   {
     message:
-      "Please navigate to the projects page to see my work, or check out the contact page to get in touch!",
+      "Please navigate to the projects page to see my work, or check out the contact page to get in touch, or peek the about page to learn a little more about me!",
     speaker: "David",
     image: "/images/avatar.png",
     imagePosition: "left",
